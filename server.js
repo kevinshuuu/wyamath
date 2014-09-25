770b1f918971ffd329752e3b80fc1824259b1fc0
Kevin Shu <kevin2041@gmail.com>
2014-09-24 22:41:41 -0500

    refactoring question generation logic to its own file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var stylus = require('stylus');
+var question = require('./lib/question');
 
 app.engine('jade', require('jade').__express);
 app.set('views', './public/views');
@@ -22,10 +23,10 @@ app.use('/', index);
 
 var users = {};
 
-var question = generateQuestion();
+var current_question = question.generateQuestion();
 function generateAndEmitQuestion() {
-  question = generateQuestion();
-  io.sockets.emit('new question', question);
+  current_question = question.generateQuestion();
+  io.sockets.emit('new question', current_question);
 }
 
 var questionInterval = setInterval(generateAndEmitQuestion, 5000);
@@ -33,16 +34,16 @@ var questionInterval = setInterval(generateAndEmitQuestion, 5000);
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.emit('connected');
-  socket.emit('new question', question);
+  socket.emit('new question', current_question);
   socket.emit('user list', users);
   socket.on('submit answer', function(data) {
-    if (data.answer == question.answer) {
+    if (data.answer == current_question.answer) {
       users[socket.username] += 1;
       io.sockets.emit('user list', users);
 
       socket.emit('correct answer');
-      question = generateQuestion();
-      io.sockets.emit('new question', question);
+      current_question = question.generateQuestion();
+      io.sockets.emit('new question', current_question);
 
       clearInterval(questionInterval);
       questionInterval = setInterval(generateAndEmitQuestion, 5000);
@@ -71,70 +72,3 @@ io.on('connection', function(socket){
 http.listen(port, function(){
     console.log('Server listening on port %d', port);
 });
-
-//helpers
-var ADDITION = 0;
-var SUBTRACTION = 1;
-var MULTIPLICATION = 2;
-var DIVISION = 3;
-
-function generateQuestion() {
-  switch(getRandomInt(0,3)) {
-    case 0:
-      return generateAddition(
-          getRandomInt(1,10), 
-          getRandomInt(5,20)
-          );
-      break;
-    case 1:
-      return generateSubtraction(
-          getRandomInt(5,15), 
-          getRandomInt(5,10)
-          );
-      break;
-    case 2:
-      return generateMultiplication(
-          getRandomInt(1,10), 
-          getRandomInt(5,20)
-          );
-      break;
-    case 3:
-      return generateDivision(
-          getRandomInt(1,10), 
-          getRandomInt(5,20)
-          );
-      break;
-  }
-}
-
-function generateAddition(lvalue, rvalue) {
-  var question = { question: lvalue+" + "+rvalue,
-    answer: lvalue+rvalue
-  }
-  return question;
-}
-
-function generateSubtraction(lvalue, rvalue) {
-  var question = { question: lvalue+" - "+rvalue,
-    answer: lvalue-rvalue
-  }
-  return question;
-}
-
-function generateMultiplication(lvalue, rvalue) {
-  var question = { question: lvalue+" * "+rvalue,
-    answer: lvalue*rvalue
-  }
-  return question;
-}
-
-function generateDivision(lvalue, rvalue) {
-  var question = { question: lvalue*rvalue+" / "+rvalue,
-    answer: lvalue
-  }
-  return question;
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max-min + 1)) + min;
-}
