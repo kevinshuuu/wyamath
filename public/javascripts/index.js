e4364cfa509fd7f64341ae98937ef960dd02563d
Kevin Shu <kevin2041@gmail.com>
2014-09-27 17:00:05 -0500

    reverting to a more basic version of the application

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,113 +4,59 @@ function mainController($scope, $http) {
   $scope.problem = "";
   $scope.status = "";
   $scope.users = [];
-  $scope.rooms = [];
-  $scope.current_room = '/general';
-  var main_socket = io.connect('/');
-  var socket = io('/general');
-  var socket_2 = io('/general-2');
-  var socket_3 = io('/general-3');
-  var socket_4 = io('/general-4');
+  var socket = io.connect();
 
-  main_socket.on('room list', function(data) {
-    console.log(data.rooms);
-    $scope.rooms = data.rooms;
+  socket.on('user list', function(data) {
+    var processed_users = []
+    for (var key in data) {
+      processed_users.push({
+        'user': key,
+        'score': data[key]
+      });
+    }
+    console.log(processed_users);
+    $scope.users = processed_users;
     $scope.$apply();
-    
-    $('.room-link').click(function() {
-      $scope.current_room = $(this).html();
-      $scope.$apply();
-    });
   });
 
-  function bindSockets(socket) {
-    var self = this;
-    self.socket = socket;
-    self.socket_room = socket.nsp;
-    self.socket.on('user list', function(data) {
-      if (self.socket_room === $scope.current_room) {
-        var processed_users = []
-        for (var key in data) {
-          processed_users.push({
-            'user': key,
-            'score': data[key]
-          });
-        }
-        console.log(processed_users);
-        $scope.users = processed_users;
-        $scope.$apply();
+  socket.on('new question', function(data) {
+    $scope.problem = data.question;
+    $scope.$apply();
+  });
+
+  socket.on('correct answer', function(data) {
+    $scope.status = "correct!";
+    $scope.$apply();
+
+    setTimeout(function() {
+      $scope.status = "";
+      $scope.$apply();
+    }, 1000);
+  });
+
+  socket.on('submitted answer', function(data) {
+    $('.chat-messages-list').append(
+      $('<li>').addClass(data.correctness).append(
+        $('<span>').append(data.username+": "+data.submitted)
+        )
+      );
+    $('.chat-area')[0].scrollTop = $('.chat-area')[0].scrollHeight;
+  });
+
+  $('.chat-input').focus(function() {
+    if (username === "") {
+      username = escape(prompt('enter a username'));
+      socket.emit('set username', {username: username});
+    }
+  });
+
+  $('.chat-input').keyup(function (e) {
+    if (e.keyCode == 13) {
+      var answer = escape($('.chat-input').val());
+      if(answer !== "") {
+        socket.emit('submit answer', {answer: answer});
       }
-    });
-
-    self.socket.on('connected', function(data) {
-      if (self.socket_room === $scope.current_room) {
-        console.log('connected');
-      }
-    });
-
-    self.socket.on('new question', function(data) {
-      console.log('receiving question from %s', self.socket_room);
-      if (self.socket_room === $scope.current_room) {
-        $scope.problem = data.question;
-        $scope.$apply();
-      }
-    });
-
-    self.socket.on('correct answer', function(data) {
-      if (self.socket_room === $scope.current_room) {
-        $scope.status = "correct!";
-        $scope.$apply();
-
-        setTimeout(function() {
-          $scope.status = "";
-          $scope.$apply();
-        }, 1000);
-      }
-    });
-
-    self.socket.on('submitted answer', function(data) {
-      if (self.socket_room === $scope.current_room) {
-        $('.chat-messages-list').append(
-          $('<li>').addClass(data.correctness).append(
-            $('<span>').append(data.username+": "+data.submitted)
-            )
-          );
-        $('.chat-area')[0].scrollTop = $('.chat-area')[0].scrollHeight;
-      }
-    });
-
-    $('.chat-input').focus(function() {
-      if (username === "") {
-        username = S(prompt('enter a username')).stripTags().s;
-
-        if (socket.nsp === $scope.current_room) {
-          console.log('setting username');
-          self.socket.emit('set username', {username: username});
-        }
-      }
-    });
-
-    $('.chat-input').keyup(function (e) {
-      if (e.keyCode == 13) {
-        var answer = S($('.chat-input').val()).stripTags().s;
-        if(answer !== "") {
-          if (socket.nsp === $scope.current_room) {
-            self.socket.emit('submit answer', {answer: answer});
-          }
-        }
-        $('.chat-input').val("");
-      }
-    });
-  }
-
-  var sock1 = new bindSockets(socket);
-  var sock2 = new bindSockets(socket_2);
-  var sock3 = new bindSockets(socket_4);
-  var sock4 = new bindSockets(socket_3);
-
-  window.sockets = [];
-  window.sockets.push(sock1);
-  window.sockets.push(sock2);
-  window.sockets.push(sock3);
-  window.sockets.push(sock4);
+      $('.chat-input').val("");
+    }
+  });
 }
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var stylus = require('stylus');
-var room = require('./lib/room');
+var question = require('./lib/question');
+var index = require('./routes/index');
 
 app.engine('jade', require('jade').__express);
 app.set('views', './public/views');
@@ -18,28 +19,56 @@ app.use(stylus.middleware({
 }));
 app.use(express.static(__dirname + '/public'));
 
-var index = require('./routes/index');
 app.use('/', index);
 
-var rooms = [];
-var room_names = [];
-createRoom('/general');
-createRoom('/general-2');
-createRoom('/general-3');
-createRoom('/general-4');
+var users = {};
 
-io.on('connection', function(socket) {
-  socket.emit('room list', {rooms: room_names});
+var current_question = question.generateQuestion();
+function generateAndEmitQuestion() {
+  current_question = question.generateQuestion();
+  io.emit('new question', current_question);
+}
+
+var questionInterval = setInterval(generateAndEmitQuestion, 5000);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.emit('connected');
+  socket.emit('new question', current_question);
+  socket.emit('user list', users);
+  socket.on('submit answer', function(data) {
+    if (data.answer == current_question.answer) {
+      users[socket.username] += 1;
+      io.emit('user list', users);
+
+      socket.emit('correct answer');
+      current_question = question.generateQuestion();
+      io.emit('new question', current_question);
+
+      clearInterval(questionInterval);
+      questionInterval = setInterval(generateAndEmitQuestion, 5000);
+      io.emit('submitted answer', {
+        submitted: data.answer, 
+        username: socket.username, 
+        correctness: 'correct'
+      });
+    } else {
+      io.emit('submitted answer', {
+        submitted: data.answer, 
+        username: socket.username, 
+        correctness: 'incorrect'
+      });
+    }
+  });
+
+  socket.on('set username', function(data) {
+    socket.username = data.username;
+    users[data.username] = 0;
+
+    io.emit('user list', users);
+  });
 });
-
 
 http.listen(port, function(){
     console.log('Server listening on port %d', port);
 });
-
-//helpers
-function createRoom(name) {
-  rooms.push(new room(io.of(name)));
-  room_names.push(name);
-  io.sockets.emit('room list', {rooms: room_names});
-}
